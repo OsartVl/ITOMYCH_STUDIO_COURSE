288e2e723a82805384eef2b349038fcea76e69d0
Osadchiy Vladislav <93037813+OsartVl@users.noreply.github.com>
2022-06-18 23:49:11 +0300

    First task

--- src/js/index.js
+++ src/js/index.js
@@ -1,75 +1,17 @@
-console.log(`Варіант 1`);
+// Write a function that takes two values, say a and b, as arguments
 
-let Multiples3 = [];
+// Return true if the two values are equal and of the same type
 
-for (let i = 1; i <= 100; i++) {
-    if (i % 3 == 0 ) {
-        Multiples3.push(i);
+function myFunction(a, b) {
+    return
+    if (a === b || a == b) {
     }
-}
-console.log("Числа кратні 3", Multiples3);
-
-let Multiples5 = [];
-for (let i = 1; i <= 100; i++) {
-    if (i % 5 == 0  ) {
-        Multiples5.push(i);
     }
-}
-console.log("Числа кратні 5", Multiples5);
-
-let Multiples15 = [];
-for (let i = 1; i <= 100; i++) {
-    if (i % 15 == 0 ) {
-        Multiples15.push(i);
-    }
-}
-console.log("Числа кратні 15", Multiples15);
-
-let Multiples3_5_15 = [];
-for (let i = 1; i <= 100; i++) {
-    if (i % 3 == 0 || i % 5 == 0 || i % 15 == 0) {
-        Multiples3_5_15.push(i);
-    }
-}
-console.log("Числа кратні 3, 5, 15", Multiples3_5_15);
-
-// ! 2 Варіант
-
-function Multiples(items, multiples) {
-    let array = [];
-    for (let i = 1; i <= items; i++) {
-        if (i % multiples == 0 ) {
-            array.push(i);
-        }
-    }
-    console.log(`Числа кратні`, String(multiples), array);
-}
-
-console.log(`Варіант 2`);
-
-Multiples(100, 3);
-Multiples(100, 5);
-Multiples(100, 15);
-
-// ! 3 Варіант
-
-console.log(`Варіант 3`);
-
-let Mult3 = [],
-    Mult5 = [],
-    Mult15= [];
-for (let i = 1; i <= 100; i++) {
-    if (i % 3 == 0 ) {
-        Mult3.push(i)
-        
-    }
-    if (i % 5 == 0) {
-        Mult5.push(i)
-    }
-    if (i % 15 == 0) {
-        Mult15.push(i)
-    }
-}
-console.log("Числа кратні 3", Mult3);
-console.log("Числа кратні 5", Mult5);
-console.log("Числа кратні 15", Mult15);
+    
+    myFunction(2, 3) // Expected false
+    
+    myFunction(3, 3) //  Expected true
+    
+    myFunction(1, '1') // Expected false
+    
+    myFunction('10', '10') // Expected true
