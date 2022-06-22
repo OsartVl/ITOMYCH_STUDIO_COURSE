e8f88d4a3fa63fb4fa5aedef4e87793ef0da9cd1
Osadchiy Vladislav <93037813+OsartVl@users.noreply.github.com>
2022-06-22 21:13:17 +0300

    New implementation of the for

--- src/js/index.js
+++ src/js/index.js
@@ -1,40 +1,5 @@
+//  1 Варіант
 console.log(`Варіант 1`);
-
-let Multiples3 = [];
-
-for (let i = 1; i <= 100; i++) {
-    if (i % 3 == 0 ) {
-        Multiples3.push(i);
-    }
-}
-console.log("Числа кратні 3", Multiples3);
-
-let Multiples5 = [];
-for (let i = 1; i <= 100; i++) {
-    if (i % 5 == 0  ) {
-        Multiples5.push(i);
-    }
-}
-console.log("Числа кратні 5", Multiples5);
-
-let Multiples15 = [];
-for (let i = 1; i <= 100; i++) {
-    if (i % 15 == 0 ) {
-        Multiples15.push(i);
-    }
-}
-console.log("Числа кратні 15", Multiples15);
-
-let Multiples3_5_15 = [];
-for (let i = 1; i <= 100; i++) {
-    if (i % 3 == 0 || i % 5 == 0 || i % 15 == 0) {
-        Multiples3_5_15.push(i);
-    }
-}
-console.log("Числа кратні 3, 5, 15", Multiples3_5_15);
-
-// ! 2 Варіант
-
 function Multiples(items, multiples) {
     let array = [];
     for (let i = 1; i <= items; i++) {
@@ -45,31 +10,24 @@ function Multiples(items, multiples) {
     console.log(`Числа кратні`, String(multiples), array);
 }
 
-console.log(`Варіант 2`);
-
 Multiples(100, 3);
 Multiples(100, 5);
 Multiples(100, 15);
 
-// ! 3 Варіант
+// 2 Варіант
 
-console.log(`Варіант 3`);
+console.log(`Варіант 2`);
 
-let Mult3 = [],
-    Mult5 = [],
-    Mult15= [];
+
 for (let i = 1; i <= 100; i++) {
     if (i % 3 == 0 ) {
-        Mult3.push(i)
+        console.log("Числа кратні 3",  i);
         
     }
     if (i % 5 == 0) {
-        Mult5.push(i)
+        console.log("Числа кратні 5", i);
     }
     if (i % 15 == 0) {
-        Mult15.push(i)
+        console.log("Числа кратні 15", i);
     }
 }
-console.log("Числа кратні 3", Mult3);
-console.log("Числа кратні 5", Mult5);
-console.log("Числа кратні 15", Mult15);
