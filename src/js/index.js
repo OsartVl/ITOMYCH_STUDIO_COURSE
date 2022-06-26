52076ce3770191186a0f8ac67f04c57aee47ca33
Osadchiy Vladislav <93037813+OsartVl@users.noreply.github.com>
2022-06-26 21:58:59 +0300

    1

--- src/js/index.js
+++ src/js/index.js
@@ -1,32 +1,38 @@
-let user = {
-    firstName: 'Alex',
-    lastName: 'Crypto',
-    age: 18,
-    email: 'alex.crypto07@gmail.com',
-    password: 'Alw32ffgrx33t',
-    getUserEmail() {
-        if (this.email == 0) {
-            return 'Введіть ваш email';
-        } else {
-            return this.email;
-        }
-    },
-    userAge() {
-        if (this.age < 18) {
-            console.log('Вам має бути більше 18 років !');
-        } else {
-            console.log('Ви маєте підтвердити свої данні !')
-        }
-    },
-    checkPassword() {
-        if (this.password.length < 6) {
-            return 'Пароль має місти більше 6 знаків';
-        } else {
-            return 'Все добре, можна рухатись далі '
-        }
-    }
+// **Homework**
 
+
+// Write a function that takes an array of strings as argument
+// Sort the array elements alphabetically
+// Return the result
+
+function myFunction(arr) {
+  let sort = []
+  return sort
 }
 
-const getUser = user.userAge
-getUser.call(user)
+myFunction([‘b’, ‘c’, ‘d’, ‘a’]) // Expected [‘a’, ‘b’, ‘c’, ‘d’]
+// myFunction([‘z’, ‘c’, ‘d’, ‘a’, ‘y’, ‘a’, ‘w’] )// Expected [‘a’, ‘a’, ‘c’, ‘d’, ‘w’, ‘y’, ‘z’]
+
+// // Write a function that takes an array (a) and a number (b) as arguments
+// // Sum up all array elements with a value greater than b
+// // Return the sum
+// function myFunction(a, b) {
+//   return
+// }
+
+// myFunction([1, 2, 3, 4, 5, 6, 7], 2) Expected 25
+// myFunction([-10, -11, -3, 1, -4], -3) Expected 1
+// myFunction([78, 99, 100, 101, 401], 99) Expected 602
+
+// // Write a function that takes two arrays as arguments
+// // Merge both arrays and remove duplicate values
+// // Sort the merge result in ascending order
+// // Return the resulting array
+
+// function myFunction(a, b) {
+
+//   return
+// }
+
+// myFunction([1, 2, 3], [3, 4, 5]) Expected [ 1, 2, 3, 4, 5 ]
+// myFunction([-10, 22, 333, 42], [-11, 5, 22, 41, 42]) Expected [ -11, -10, 5, 22, 41,  42, 333]
