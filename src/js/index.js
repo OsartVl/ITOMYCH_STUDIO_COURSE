f77d7bf96c003923337f83a5da1ec80286811548
Osadchiy Vladislav <93037813+OsartVl@users.noreply.github.com>
2022-06-27 00:56:38 +0300

    2

--- src/js/index.js
+++ src/js/index.js
@@ -1,32 +1,41 @@
-let user = {
-    firstName: 'Alex',
-    lastName: 'Crypto',
-    age: 18,
-    email: 'alex.crypto07@gmail.com',
-    password: 'Alw32ffgrx33t',
-    getUserEmail() {
-        if (this.email == 0) {
-            return 'Введіть ваш email';
-        } else {
-            return this.email;
+function User(firstName, lastName, age, email, password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.email = email;
+        this.password = password;
+        getUserEmail = function() {
+            if (this.email == 0) {
+             return 'Введіть ваш email';
+            } else {
+                return this.email;
+            }
         }
-    },
-    userAge() {
-        if (this.age < 18) {
-            console.log('Вам має бути більше 18 років !');
-        } else {
-            console.log('Ви маєте підтвердити свої данні !')
+        userAge = function() {
+            if (this.age < 18) {
+                return 'Вам має бути більше 18 років !'
+            } else {
+                return 'Ви маєте підтвердити свої данні !'
+            }
         }
-    },
-    checkPassword() {
-        if (this.password.length < 6) {
-            return 'Пароль має місти більше 6 знаків';
-        } else {
-            return 'Все добре, можна рухатись далі '
-        }
-    }
+        checkPassword = function() {
+            if (this.password.length < 6) {
+                return 'Пароль має місти більше 6 знаків';
+            } else {
+                return 'Все добре, можна рухатись далі '
+            }
+        };
 
 }
 
-const getUser = user.userAge
-getUser.call(user)
+let Sign_Up_User = new User();
+
+Sign_Up_User = {
+    firstName: 'Vlad',
+    lastName: 'Osadchyi',
+    age: 18,
+    email: 'vlad.osadchiy00@gmail.com',
+    getUserEmail(){}, 
+}
+
+console.log(User.getUserEmail());
