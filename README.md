0219c1d89bb6e58a59b55e3080cb3027ff6d86f7
Osadchiy Vladislav <93037813+OsartVl@users.noreply.github.com>
2022-06-26 14:23:13 +0300

    first commit

--- README.md
+++
@@ -1,1 +0,0 @@
-![image](https://user-images.githubusercontent.com/93037813/174457257-3e3d3c8d-2fc0-42fd-9c10-ece00bf3180c.png)
--- src/js/index.js
+++ src/js/index.js
@@ -1,49 +1,32 @@
-// Write a function that takes two values, say a and b, as arguments
-
-// Return true if the two values are equal and of the same type
-
-function myFunction(a, b) {
-
-    if ( a === b && a == b) {
-        console.log(true);
-    } else { console.log(false); }
-    
-    return
+let user = {
+    firstName: 'Alex',
+    lastName: 'Crypto',
+    age: 18,
+    email: 'alex.crypto07@gmail.com',
+    password: 'Alw32ffgrx33t',
+    getUserEmail() {
+        if (this.email == 0) {
+            return 'Введіть ваш email';
+        } else {
+            return this.email;
+        }
+    },
+    userAge() {
+        if (this.age < 18) {
+            console.log('Вам має бути більше 18 років !');
+        } else {
+            console.log('Ви маєте підтвердити свої данні !')
+        }
+    },
+    checkPassword() {
+        if (this.password.length < 6) {
+            return 'Пароль має місти більше 6 знаків';
+        } else {
+            return 'Все добре, можна рухатись далі '
+        }
     }
-    
-myFunction(2, 3); // Expected false
-    
-myFunction(3, 3); //  Expected true
-    
-myFunction(1, '1'); // Expected false
-
-myFunction('10', '10'); // Expected true
-
-// # **Homework2**
-
-// Write a function that takes two numbers, say x and y, as arguments
-
-// Check if x is divisible by y
-
-// If yes, return x
-
-// If not, return the next higher natural number that is divisible by y
-
-function myFunction2 (x, y) {
-    if (x % y == 0) { 
-        console.log(x);
-    }
-    else if (x % y != 0) {
-        return x + myFunction2(x + 1, y);
-    }
-return
 
 }
 
-myFunction2(1, 23); // Expected 23
-
-myFunction2(23, 23); // Expected 23
-
-myFunction2(7, 3); // Expected 9
-
-myFunction2(-5, 7); // Expected 0
+const getUser = user.userAge
+getUser.call(user)
