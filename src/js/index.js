0144580e55fa1e88f40ed2e2ba71db6668fe08ba
Osadchiy Vladislav <93037813+OsartVl@users.noreply.github.com>
2022-06-27 19:11:30 +0300

    2

--- src/js/index.js
+++ src/js/index.js
@@ -1,41 +1,86 @@
-function User(firstName, lastName, age, email, password) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.age = age;
-        this.email = email;
-        this.password = password;
-        getUserEmail = function() {
-            if (this.email == 0) {
-             return 'Введіть ваш email';
-            } else {
-                return this.email;
-            }
+let User = {
+    firstName: 'firstName',
+    lastName: 'lastName',
+    age: 18,
+    email: '',
+    password: 'password',
+    getUserEmail(email) {
+        if (email == 0) {   
+            console.log('Введіть ваш email');   
+        } else {
+            console.log(email);
         }
-        userAge = function() {
-            if (this.age < 18) {
-                return 'Вам має бути більше 18 років !'
-            } else {
-                return 'Ви маєте підтвердити свої данні !'
-            }
+    },
+    userAge() {
+        if (this.age < 18) {
+            console.log('Вам має бути більше 18 років !')
+        } else {
+            console.log('Ви маєте підтвердити свої данні !')
         }
-        checkPassword = function() {
-            if (this.password.length < 6) {
-                return 'Пароль має місти більше 6 знаків';
-            } else {
-                return 'Все добре, можна рухатись далі '
-            }
-        };
+    },
+    checkPassword() {
+        if (this.password.length < 6) {
+            console.log('Пароль має місти більше 6 знаків');
+        } else {
+            console.log('Все добре, можна рухатись далі ')
+        }
+    },
 
 }
 
-let Sign_Up_User = new User();
+function GetUser(firstName, lastName, age, email, password){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.email = 'email';
+    this.password = 'password';
+    this.getUserEmail = function() {
+        if (this.email == 0) {
+            console.log('Введіть ваш email');
+        } else {
+            console.log(this.email);
+        }
+    };
+    this.userAge = function () {
+        if (this.age < 18) {
+            console.log('Вам має бути більше 18 років !')
+        } else {
+            console.log('Ви маєте підтвердити свої данні !')
+        }
+    };
+    checkPassword = function () {
+        if (this.password.length < 6) {
+            console.log('Пароль має місти більше 6 знаків');
+        } else {
+            console.log('Все добре, можна рухатись далі ')
+        }
+    };
+}
+
+
+
+let Sign_Up_User = User;
+
+Sign_Up_User.userAge()
+Sign_Up_User.getUserEmail('vlad.osadchiy00@gmail.com')
+Sign_Up_User.checkPassword()
 
 Sign_Up_User = {
     firstName: 'Vlad',
     lastName: 'Osadchyi',
     age: 18,
     email: 'vlad.osadchiy00@gmail.com',
-    getUserEmail(){}, 
+    getUserEmail() { },
+    userAge() { },
+    checkPassword(){},
 }
 
-console.log(User.getUserEmail());
+const ConstuctorUser = new GetUser();
+
+ConstuctorUser.firstName = 'Vlad',
+ConstuctorUser.lastName = 'Osadchyi',
+ConstuctorUser.age = 18,
+ConstuctorUser.email = 'vlad.osadchiy00@gmail.com',
+
+console.log(Sign_Up_User);
+console.log(ConstuctorUser);
