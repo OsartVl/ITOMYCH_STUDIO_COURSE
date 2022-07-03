59786778bc8ff549e1ec2b4f95c6969f792e6d7d
Osadchiy Vladislav <93037813+OsartVl@users.noreply.github.com>
2022-07-03 11:08:45 +0300

    First commit

--- src/js/index.js
+++ src/js/index.js
@@ -6,16 +6,16 @@
 // Return the result
 
 function myFunction(arr) {
-  let sort = []
-  return sort
+    arr.sort() 
 }
 
-myFunction([‘b’, ‘c’, ‘d’, ‘a’]) // Expected [‘a’, ‘b’, ‘c’, ‘d’]
-// myFunction([‘z’, ‘c’, ‘d’, ‘a’, ‘y’, ‘a’, ‘w’] )// Expected [‘a’, ‘a’, ‘c’, ‘d’, ‘w’, ‘y’, ‘z’]
+myFunction(['b', 'c', 'd', 'a']) // Expected [‘a’, ‘b’, ‘c’, ‘d’]
+myFunction(['z', 'c', 'd', 'a', 'y', 'a', 'w'] )// Expected [‘a’, ‘a’, ‘c’, ‘d’, ‘w’, ‘y’, ‘z’]
 
-// // Write a function that takes an array (a) and a number (b) as arguments
-// // Sum up all array elements with a value greater than b
-// // Return the sum
+// Write a function that takes an array (a) and a number (b) as arguments
+// Sum up all array elements with a value greater than b
+// Return the sum
+
 // function myFunction(a, b) {
 //   return
 // }
@@ -24,10 +24,10 @@ myFunction([‘b’, ‘c’, ‘d’, ‘a’]) // Expected [‘a’, ‘b’, 
 // myFunction([-10, -11, -3, 1, -4], -3) Expected 1
 // myFunction([78, 99, 100, 101, 401], 99) Expected 602
 
-// // Write a function that takes two arrays as arguments
-// // Merge both arrays and remove duplicate values
-// // Sort the merge result in ascending order
-// // Return the resulting array
+// Write a function that takes two arrays as arguments
+// Merge both arrays and remove duplicate values
+// Sort the merge result in ascending order
+// Return the resulting array
 
 // function myFunction(a, b) {
 
@@ -35,4 +35,4 @@ myFunction([‘b’, ‘c’, ‘d’, ‘a’]) // Expected [‘a’, ‘b’, 
 // }
 
 // myFunction([1, 2, 3], [3, 4, 5]) Expected [ 1, 2, 3, 4, 5 ]
-// myFunction([-10, 22, 333, 42], [-11, 5, 22, 41, 42]) Expected [ -11, -10, 5, 22, 41,  42, 333]
+// myFunction([-10, 22, 333, 42], [-11, 5, 22, 41, 42])  Expected [ -11, -10, 5, 22, 41,  42, 333]
