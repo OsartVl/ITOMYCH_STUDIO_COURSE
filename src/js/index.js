f04f15b69fc598e7d7b748dd4ad385b15ae11181
Osadchiy Vladislav <93037813+OsartVl@users.noreply.github.com>
2022-07-08 14:48:41 +0300

    start

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,43 @@
-let tablet = {
-    sizeCentimeter: 17,
-    ram: 8 + " gb",
-    cpu: "Mediatek",
-    number_of_cores: 8,
-    cameraPresence: true,
 
-    display_diagonal() {
-        let diagonal = Math.ceil(this.sizeCentimeter / 2.14);
-        return diagonal
-    },
-    takePhoto() {
+
+function myFunction (obj) { 
+    let personalInfo = {
+        firstName: obj.fn,
+        lastName: obj.ln,
+        size: obj.size,
+        weight: obj.weight,
     }
-}
 
-console.log(tablet.display_diagonal())
+    if (personalInfo.firstName != undefined) {
+        personalInfo.size = obj.size + "cm";
+    }
+    if (obj.size == undefined) {
+        delete personalInfo.size;
+    } else {
+        personalInfo.size = obj.size + "cm";
+    }
+    if (obj.weight == undefined) {
+        delete personalInfo.weight
+        
+    } else {
+        personalInfo.weight = obj.weight + "kg";
+    }
+
+    return personalInfo
+}
+    
+    console.log(myFunction({fn: 'Lisa', ln: 'Müller', age: 17, size: 175, weight: 67}))
+    
+    // Expected {fn: 'Lisa', ln: 'Müller', size: '175cm', weight: '67kg'}
+    
+    console.log(myFunction({fn: 'Martin', ln: 'Harper', age: 26, email: 'martin.harper@test.de', weight: 102}))
+    
+    // Expected {fn: 'Martin', ln: 'Harper', weight: '102kg'}
+    
+    console.log(myFunction({ fn: 'Andrew', ln: 'Harper', age: 81, size: 175, weight: 71 }))
+    
+    // Expected {fn: 'Andrew', ln: 'Harper', size: '175cm', weight: '71kg'}
+    
+    console.log(myFunction({fn: 'Matthew', ln: 'Müller', age: 19, email: 'matthew@mueller.de'}))    
+    
+    // Expected {fn: 'Matthew', ln: 'Müller'}
