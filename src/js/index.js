d7ff88dba0317000db7c31e69fb1bdc3e0f82b68
Osadchiy Vladislav <93037813+OsartVl@users.noreply.github.com>
2022-07-08 21:34:41 +0300

    maybe final

--- src/js/index.js
+++ src/js/index.js
@@ -1,43 +1,22 @@
 
 
-function myFunction (obj) { 
-    let personalInfo = {
-        firstName: obj.fn,
-        lastName: obj.ln,
-        size: obj.size,
-        weight: obj.weight,
+function myFunction(obj) { 
+    let newObj = {
+        firstName: obj.fn, lastName: obj.ln
+    };
+    if (obj.size) {
+        newObj.size = obj.size + "cm"
     }
-
-    if (personalInfo.firstName != undefined) {
-        personalInfo.size = obj.size + "cm";
+    if (obj.weight) {
+        newObj.weight = obj.weight + "kg"
     }
-    if (obj.size == undefined) {
-        delete personalInfo.size;
-    } else {
-        personalInfo.size = obj.size + "cm";
-    }
-    if (obj.weight == undefined) {
-        delete personalInfo.weight
-        
-    } else {
-        personalInfo.weight = obj.weight + "kg";
-    }
-
-    return personalInfo
+    return newObj
 }
-    
-    console.log(myFunction({fn: 'Lisa', ln: 'Müller', age: 17, size: 175, weight: 67}))
-    
+    myFunction({fn: 'Lisa', ln: 'Müller', age: 17, size: 175, weight: 67})
     // Expected {fn: 'Lisa', ln: 'Müller', size: '175cm', weight: '67kg'}
-    
-    console.log(myFunction({fn: 'Martin', ln: 'Harper', age: 26, email: 'martin.harper@test.de', weight: 102}))
-    
+    myFunction({fn: 'Martin', ln: 'Harper', age: 26, email: 'martin.harper@test.de', weight: 102})
     // Expected {fn: 'Martin', ln: 'Harper', weight: '102kg'}
-    
     console.log(myFunction({ fn: 'Andrew', ln: 'Harper', age: 81, size: 175, weight: 71 }))
-    
     // Expected {fn: 'Andrew', ln: 'Harper', size: '175cm', weight: '71kg'}
-    
-    console.log(myFunction({fn: 'Matthew', ln: 'Müller', age: 19, email: 'matthew@mueller.de'}))    
-    
+    myFunction({fn: 'Matthew', ln: 'Müller', age: 19, email: 'matthew@mueller.de'})    
     // Expected {fn: 'Matthew', ln: 'Müller'}
