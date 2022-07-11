1fc69dc8cdd6ba3f8be07c82c168df6861352ce2
Osadchiy Vladislav <93037813+OsartVl@users.noreply.github.com>
2022-07-11 18:45:55 +0300

    new animal final work

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,8 @@
-let Tiger = {
-    furСolor: "white",
+let Lion = {
     weight: 250,
     age: 3,
-    name: "Lion",
-    type: "Chordates",
+    name: "",
+    type: "",
     speed: 80,
     bio() {
         alert(`${this.name} наледить до типу ${this.type}, вид хижих ссавців з роду пантер родини котових ! Це другий за величиною вид родини після тигра. `);
@@ -17,8 +16,33 @@ let Tiger = {
 
 }
 
-console.log(Tiger.run())
-console.log(Tiger.bio())
-console.log(Tiger.maxWeight())
+let newTiger = Lion;
 
+newTiger.weight = 300;
+newTiger.name = 'Lion';
+newTiger.age = 15;
+newTiger.type = "Chordates";
+newTiger.speed = 90;
+newTiger.bio();
+newTiger.run();
+newTiger.maxWeight();
 
+function LionFunction(weight, name, age, type, speed ) {
+    this.weight = weight;
+    this.weight = name;
+    this.age = age;
+    this.type = type;
+    this.speed = speed;
+    function bio(){
+        alert(`${this.name} наледить до типу ${this.type}, вид хижих ссавців з роду пантер родини котових ! Це другий за величиною вид родини після тигра. `);
+    };
+    function run() {
+        alert(`${this.name} може бігти з максимальною швидкістю ${this.speed} км ! `);
+    };
+    function maxWeight() {
+        alert(`${this.name} максимальна вага складає ${this.weight} кг `)
+    };
+}
+
+const LionConstructor = new LionFunction(250, "Lion 2",20, "Chordates", 85);
+console.log(LionConstructor);
