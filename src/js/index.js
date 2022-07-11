2745e2348af6d1f270b8bbc180dff9d1b0adeacc
Osadchiy Vladislav <93037813+OsartVl@users.noreply.github.com>
2022-07-11 17:35:34 +0300

    final

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,24 @@
-let tablet = {
-    sizeCentimeter: 17,
-    ram: 8 + " gb",
-    cpu: "Mediatek",
-    number_of_cores: 8,
-    cameraPresence: true,
-
-    display_diagonal() {
-        let diagonal = Math.ceil(this.sizeCentimeter / 2.14);
-        return diagonal
+let Tiger = {
+    furСolor: "white",
+    weight: 250,
+    age: 3,
+    name: "Lion",
+    type: "Chordates",
+    speed: 80,
+    bio() {
+        alert(`${this.name} наледить до типу ${this.type}, вид хижих ссавців з роду пантер родини котових ! Це другий за величиною вид родини після тигра. `);
     },
-    takePhoto() {
+    run() {
+        alert(`${this.name} може бігти з максимальною швидкістю ${this.speed} км ! `);
+    },
+    maxWeight() {
+        alert(`${this.name} максимальна вага складає ${this.weight} кг `)
     }
+
 }
 
-console.log(tablet.display_diagonal())
+console.log(Tiger.run())
+console.log(Tiger.bio())
+console.log(Tiger.maxWeight())
+
+
