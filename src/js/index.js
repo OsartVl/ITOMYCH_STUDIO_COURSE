3c3ad6511e4f26a07c34a39072cd0b9cc1673182
Osadchiy Vladislav <93037813+OsartVl@users.noreply.github.com>
2022-07-11 22:46:35 +0300

    First

--- src/js/index.js
+++ src/js/index.js
@@ -1,48 +1,28 @@
-let Lion = {
-    weight: 250,
-    age: 3,
-    name: "",
-    type: "",
-    speed: 80,
-    bio() {
-        alert(`${this.name} наледить до типу ${this.type}, вид хижих ссавців з роду пантер родини котових ! Це другий за величиною вид родини після тигра. `);
-    },
-    run() {
-        alert(`${this.name} може бігти з максимальною швидкістю ${this.speed} км ! `);
-    },
-    maxWeight() {
-        alert(`${this.name} максимальна вага складає ${this.weight} кг `)
+class Animal {
+    constructor(weight, name, age, type, speed ) {
+        this._name = name;
+        this._weight = weight;
+        this._age = age;
+        this._type = type;
+        this._speed = speed;
     }
+    get() {
+        return this.bio();
+    }
+    bio() {
+        alert(`${this._name} наледить до типу ${this._type}, вид хижих ссавців з роду пантер родини котових ! Це другий за величиною вид родини після тигра. `);
+    };
+    run() {
+        alert(`${this._name} може бігти з максимальною швидкістю ${this._speed} км ! `);
+    };
+    maxWeight() {
+        alert(`${this._name} максимальна вага складає ${this._weight} кг `)
+    };
+
 
 }
 
-let newTiger = Lion;
+const Lion = new Animal(250, "Lion 2", 20, "Chordates", 85);
+console.log(Lion.get());
+console.log(Lion);
 
-newTiger.weight = 300;
-newTiger.name = 'Lion';
-newTiger.age = 15;
-newTiger.type = "Chordates";
-newTiger.speed = 90;
-newTiger.bio();
-newTiger.run();
-newTiger.maxWeight();
-
-function LionFunction(weight, name, age, type, speed ) {
-    this.weight = weight;
-    this.weight = name;
-    this.age = age;
-    this.type = type;
-    this.speed = speed;
-    function bio(){
-        alert(`${this.name} наледить до типу ${this.type}, вид хижих ссавців з роду пантер родини котових ! Це другий за величиною вид родини після тигра. `);
-    };
-    function run() {
-        alert(`${this.name} може бігти з максимальною швидкістю ${this.speed} км ! `);
-    };
-    function maxWeight() {
-        alert(`${this.name} максимальна вага складає ${this.weight} кг `)
-    };
-}
-
-const LionConstructor = new LionFunction(250, "Lion 2",20, "Chordates", 85);
-console.log(LionConstructor);
