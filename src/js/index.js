3eba3bb607e748666654fef5a6a3b72c582c6faa
Osadchiy Vladislav <93037813+OsartVl@users.noreply.github.com>
2022-07-13 17:51:54 +0300

    first

--- src/js/index.js
+++ src/js/index.js
@@ -1,28 +1,14 @@
-class Animal {
-    constructor(weight, name, age, type, speed ) {
-        this._name = name;
-        this._weight = weight;
-        this._age = age;
-        this._type = type;
-        this._speed = speed;
-    }
-    get() {
-        return this.bio();
-    }
-    bio() {
-        alert(`${this._name} наледить до типу ${this._type}, вид хижих ссавців з роду пантер родини котових ! Це другий за величиною вид родини після тигра. `);
-    };
-    run() {
-        alert(`${this._name} може бігти з максимальною швидкістю ${this._speed} км ! `);
-    };
-    maxWeight() {
-        alert(`${this._name} максимальна вага складає ${this._weight} кг `)
-    };
+const requestURL = 'https://jsonplaceholder.typicode.com/posts';
 
-
+function sendRequest(method, url) {
+  return fetch(url).then(response => {
+   return response.json()
+  })
 }
 
-const Lion = new Animal(250, "Lion 2", 20, "Chordates", 85);
-console.log(Lion.get());
-console.log(Lion);
+sendRequest("GET", requestURL)
+  .then(data => {
+    console.log(data)
+  })
+  .catch(err => console.error(err))
 
